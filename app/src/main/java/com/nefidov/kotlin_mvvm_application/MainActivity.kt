615ee04a9e4b2b26007d3056c90c726e77d12f57
Нефидов Михаил <nefidov@ddplanet.ru>
2024-10-02 14:42:58 +0300

    first commit

---
+++ app/src/main/java/com/nefidov/kotlin_mvvm_application/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.nefidov.kotlin_mvvm_application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nefidov.kotlin_mvvm_application.ui.theme.Kotlin_mvvm_applicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Kotlin_mvvm_applicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kotlin_mvvm_applicationTheme {
+        Greeting("Android")
+    }
+}
